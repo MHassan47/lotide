37c912a7501abb0f008a896d3b215147dad740da
Mohamed <mhassankd35@gmail.com>
2022-05-05 00:57:37 -0500

    implementing finding a key by value using assert

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKeyByValue(object, value) {
+  const keys = Object.keys(object);
+  for (let element of keys) {
+    if (object[element] === value) return element;
+  }
+}
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
