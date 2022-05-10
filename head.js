1236067b05bcd29834e3024a727a3e864f2d0290
Mohamed <mhassankd35@gmail.com>
2022-05-09 20:17:44 -0500

    refactored head array using assert

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
+const assertEqual = require('./assertEqual');
+
 const head = function (actual, expected) {
   return actual[0];
-};
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
 };
 
 assertEqual(head([5, 6, 7]), 5);
